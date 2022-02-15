5a55b11a83f81cea06dafc3a9d3738f87a08a2c3
Zachary Rudebeck <zachary.rudebeck@gmail.com>
2022-02-15 12:21:48 -0700

    made merch cards reactive

--- frontend/src/components/MerchListing.js
+++ frontend/src/components/MerchListing.js
@@ -1,19 +1,25 @@
 import React from "react";
 import MerchContainer from "./MerchContainer";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, useMediaQuery } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  MerchListing: {
+  desktop: {
     display: "flex",
     justifyContent: 'center'
+  },
+  mobile: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: 'center'
   }
 }));
 
 function MerchListing() {
 const classes = useStyles();
+const desktop = useMediaQuery('(min-width:768px)');
 
   return (
-    <div className={ classes.MerchListing }>
+    <div className={ desktop ? classes.desktop : classes.mobile }>
       <MerchContainer/>
       <MerchContainer/>
       <MerchContainer/>

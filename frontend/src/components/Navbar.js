399fb7363525a256f28cd3d1dfb11c50bbad02ea
Luis Gomez <whoislewys@gmail.com>
2022-02-15 09:15:46 -0700

    [main] add navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  appbar: {
+    backgroundColor: "black",
+  },
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: "flex",
+  },
+  logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    // "&:hover": {
+      // color: "yellow",
+      // borderBottom: "1px solid white",
+    // },
+  },
+}));
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <AppBar className={classes.appbar} position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" className={classes.logo}>
+          J
+        </Typography>
+        <div className={classes.navlinks}>
+          <Link to="/" className={classes.link}>
+            Home
+          </Link>
+          <Link to="/about" className={classes.link}>
+            About
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { DAppProvider } from '@usedapp/core'
+import { DAppProvider } from "@usedapp/core";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,3 +13,4 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+

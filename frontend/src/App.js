d6d1dddc317aad41cdcf252ac0ae305af7e9b8a7
Zachary Rudebeck <zachary.rudebeck@gmail.com>
2022-02-15 15:41:33 -0700

    setup redeem modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./views/Home";
-import About from "./views/About";
+// import Home from "./views/Home";
+// import About from "./views/About";
 import Navbar from "./components/Navbar";
 import DrawerMenu from "./components/DrawerMenu";
 import useMediaQuery from '@mui/material/useMediaQuery';
 import useWeb3Modal from "./hooks/useWeb3Modal";
+import Redeem from "./components/Redeem"
+import { makeStyles } from "@material-ui/core";
 
 function App() {
   const desktop = useMediaQuery('(min-width:768px)');
@@ -16,10 +18,11 @@ function App() {
     <div className="App">
       {desktop && <Navbar/>}
       {!desktop && <DrawerMenu/>}
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home provider={provider} />} />
         <Route path="about" element={<About />} />
-      </Routes>
+      </Routes> */}
+      <Redeem/>
     </div>
   );
 }

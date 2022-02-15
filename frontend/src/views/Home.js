b78fc6877afe9b9ce171020762269884981b1d4b
Zachary Rudebeck <zachary.rudebeck@gmail.com>
2022-02-15 10:39:43 -0700

    added jewelry containers

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import WalletButton from "../components/WalletButton";
 import MintButton from "../components/MintButton";
+import MerchListing from "../components/MerchListing";
 
 const Home = () => {
   return (
     <div>
       <WalletButton/>
       <MintButton/>
+      <MerchListing/>
     </div>
   
 

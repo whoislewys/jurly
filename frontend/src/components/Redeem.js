0083ba3903de462b231dacab4611b7052aed181e
whoislewys <whoislewys@gmail.com>
2022-02-15 23:49:24 -0700

    [main] redeem is more materialy

--- frontend/src/components/Redeem.js
+++ frontend/src/components/Redeem.js
@@ -1,135 +1,171 @@
 import React from "react";
-import { 
-  Card, 
-  makeStyles, 
+import {
+  Card,
+  makeStyles,
   useMediaQuery,
   CardActions,
   CardContent,
+  Input,
   Typography,
   Button,
-  TextField
+  TextField,
 } from "@material-ui/core";
-import Autocomplete from "react-google-autocomplete";
-import { GOOGLE_API_KEY } from "../env"
-import { margin } from "@mui/system";
+import Autocomplete, {usePlacesWidget} from "react-google-autocomplete";
+import { GOOGLE_API_KEY } from "../env";
 
 const useStyles = makeStyles((theme) => ({
+  header: {
+    color: "white",
+  },
   desktopRedeem: {
     height: 500,
     width: 500,
     borderRadius: 14,
-    backgroundColor: '#202231'
+    backgroundColor: "#202231",
   },
   mobileRedeem: {
     height: 500,
     width: "80%",
     borderRadius: 14,
-    backgroundColor: '#202231'
+    backgroundColor: "#202231",
   },
   mobileButton: {
     width: "85%",
     borderRadius: 14,
     height: 35,
     marginTop: 10,
-    backgroundColor: '#161522',
-    borderColor: '#2e3348',
-    borderWidth: '1px'
+    backgroundColor: "#161522",
+    borderColor: "#2e3348",
+    borderWidth: "1px",
+    color: "white",
   },
   desktopButton: {
     width: 300,
     borderRadius: 14,
     height: 35,
     marginTop: 10,
-    backgroundColor: '#161522',
-    borderColor: '#2e3348',
-    borderWidth: '1px'
+    backgroundColor: "green",
+    borderColor: "#2e3348",
+    borderWidth: "1px",
   },
   redeemContainer: {
     width: "100%",
     height: "90vh",
     display: "flex",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   cardActions: {
     display: "flex",
     justifyContent: "center",
-    padding: "5%"
+    padding: "5%",
   },
   content: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
+  input: {
+    // color: "white",
+    // backgroundColor: "black",
+    width: '60%',
+    marginTop: theme.spacing(2),
+  },
+  redeemButton: {
+    backgroundColor: "black",
+    color: "white",
+  },
 }));
 
 function MerchListing() {
   const classes = useStyles();
-  const desktop = useMediaQuery('(min-width:768px)');
+  const isDesktop = useMediaQuery("(min-width:768px)");
 
-  const [email, setEmail] = React.useState('');
-  const [address, setAddress] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [country, setCountry] = React.useState("us");
   const [formValid, setFormValid] = React.useState(false);
+
+  const { ref: materialRef } = usePlacesWidget({
+    apiKey: GOOGLE_API_KEY,
+    onPlaceSelected: (place) => console.log(place),
+    inputAutocompleteValue: "country",
+    options: {
+      componentRestrictions: { country },
+      types: ['address'],
+    },
+  });
 
   const setFormvalid = () => {
     if (address.length > 0 && emailValid()) {
-      setFormValid(true)
+      setFormValid(true);
     } else {
-      setFormValid(false)
+      setFormValid(false);
     }
-  }
+  };
 
   const emailValid = () => {
-    return email.toLowerCase().match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-  }
+    return email
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
   const handleEmailInput = (e) => {
-    setEmail(e.target.value)
-    setFormvalid()
-  }
+    setEmail(e.target.value);
+    setFormvalid();
+  };
 
   const handleAddressInput = (place) => {
-    setAddress(place.formatted_address)
-    setFormvalid()
-  }
+    setAddress(place.formatted_address);
+    setFormvalid();
+  };
 
   return (
-    <div className={ classes.redeemContainer }>
-      <Card className={ desktop ? classes.desktopRedeem : classes.mobileRedeem }>
-        <CardContent className={ classes.content }>
+    <div className={classes.redeemContainer}>
+      <Card
+        className={isDesktop ? classes.desktopRedeem : classes.mobileRedeem}
+      >
+        <CardContent className={classes.content}>
           <div>
-            <Typography sx={{ fontSize: 14 }} color="#7F7F7F" gutterBottom>
-              Redeem
+            <Typography
+              variant="h4"
+              className={classes.header}
+              color="#7F7F7F"
+              gutterBottom
+            >
+              Claim your Jewelry
             </Typography>
           </div>
-          <Autocomplete
-            className={ desktop ? classes.desktopButton : classes.mobileButton }
-            apiKey={GOOGLE_API_KEY}
-            onPlaceSelected={(place) => {
-              handleAddressInput(place)
-            }}
-            options={{
-              types: ["address"],
-            }}
+
+          <TextField
+            className={classes.input}
+            fullWidth
+            color="secondary"
+            variant="outlined"
+            inputRef={materialRef}
           />
-          <input 
-            className={ desktop ? classes.desktopButton : classes.mobileButton }
-            id="outlined-basic" 
-            label="Email" 
-            variant="outlined" 
+
+          {/*TODO: make styling not look poopoo*/}
+          <TextField
+            className={classes.input}
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
             value={email}
-            onChange={e => handleEmailInput(e)}
+            onChange={(e) => handleEmailInput(e)}
           />
         </CardContent>
-        <CardActions className={ classes.cardActions }>
-          <Button 
-            className={ desktop ? classes.desktopButton : classes.mobileButton } 
-            variant="outlined" 
-            disabled={!formValid} 
+        <CardActions className={classes.cardActions}>
+          <Button
+            className={classes.redeemButton}
+            variant="outlined"
+            // disabled={!formValid}
             size="small"
-          >Redeem</Button>
+          >
+            Claim
+          </Button>
         </CardActions>
       </Card>
     </div>

e28932ad3a9733211a248db25391e3998d0bedee
whoislewys <whoislewys@gmail.com>
2022-02-18 14:35:37 -0700

    [ui-branch-2] dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 import React from "react";
 import "./App.css";
 import WalletButton from "./components/WalletButton";
@@ -7,14 +8,25 @@ import useWeb3Modal from "./hooks/useWeb3Modal";
 function App() {
   const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
 
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
   return (
     <div className="App">
-      <WalletButton
-        provider={provider}
-        loadWeb3Modal={loadWeb3Modal}
-        logoutOfWeb3Modal={logoutOfWeb3Modal}
-      />
-      <Home provider={provider}/>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline in themeprovider ensures dark mode gets applied, + other nice styling */}
+        {/* you find more: https://mui.com/components/css-baseline */}
+        <CssBaseline />
+        <WalletButton
+          provider={provider}
+          loadWeb3Modal={loadWeb3Modal}
+          logoutOfWeb3Modal={logoutOfWeb3Modal}
+        />
+        <Home provider={provider} />
+      </ThemeProvider>
     </div>
   );
 }

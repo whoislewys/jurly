89534814837a403a7333153d1df3539814e68a41
whoislewys <whoislewys@gmail.com>
2022-02-19 18:13:22 -0700

    [ui-branch-2] clippy preview

--- frontend/src/components/Mint.js
+++ frontend/src/components/Mint.js
@@ -1,55 +1,52 @@
-
-
-import { makeStyles } from '@mui/styles'
+import { makeStyles } from "@mui/styles";
 import React from "react";
 import {
   Card,
   CardActions,
   useMediaQuery,
   Typography,
-  CardContent
-} from "@mui/material"
+  CardContent,
+} from "@mui/material";
 import MintButton from "./MintButton";
 
 const useStyles = makeStyles((theme) => ({
   redeemContainer: {
-    height: '90vh',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: "90vh",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   desktopRedeem: {
-    height: 500,
     width: 500,
     borderRadius: 14,
-    backgroundColor: '#202231',
+    backgroundColor: "#202231",
   },
   mobileRedeem: {
-    height: 500,
-    width: '80%',
+    width: "80%",
     borderRadius: 14,
-    backgroundColor: '#202231',
+    backgroundColor: "#202231",
   },
   cardActions: {
-    display: 'flex',
-    justifyContent: 'center',
-    padding: '5%',
+    display: "flex",
+    justifyContent: "center",
+    padding: "5%",
+    paddingBottom: theme.spacing(2),
   },
   content: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
-}))
+}));
 
-// TODO: this code is the exact same in two places (here and Redeem.js)... 
-// Eventually it would be good to rename this to Container.js use it as a 
+// TODO: this code is the exact same in two places (here and Redeem.js)...
+// Eventually it would be good to rename this to Container.js use it as a
 // modular component, and set the data within as props. Holding off for
 // now as decoupling the logic in Redeem.js is non-trivial
 const Mint = ({ provider }) => {
-  const classes = useStyles()
-  const isDesktop = useMediaQuery('(min-width:768px)')
+  const classes = useStyles();
+  const isDesktop = useMediaQuery("(min-width:768px)");
 
   return (
     <div className={classes.redeemContainer}>
@@ -59,12 +56,18 @@ const Mint = ({ provider }) => {
         <CardContent className={classes.content}>
           <Typography variant="h4">Mint</Typography>
         </CardContent>
+        <CardContent className={classes.content}>
+          <img src={require("../assets/clippy-render.jpg")} style={{
+            width: '40vh',
+            height: '40vh',
+            }}/>
+        </CardContent>
         <CardActions className={classes.cardActions}>
           <MintButton provider={provider} />
         </CardActions>
       </Card>
     </div>
-  )
+  );
 };
 
 export default Mint;
